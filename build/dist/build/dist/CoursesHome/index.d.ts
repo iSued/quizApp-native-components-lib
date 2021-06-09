8d69f10b8e8166e5e7a0dfc1496ab1bdaf6f7645
Sued Sinani <suedsinani@protonmail.ch>
2021-06-09 11:04:54 +0200

    update

--- build/dist/build/dist/CoursesHome/index.d.ts
+++
@@ -1,5 +0,0 @@
-export var __esModule: boolean;
-export default HomeCourses;
-declare function HomeCourses({ card }: {
-    card: any;
-}): JSX.Element;
--- build/dist/build/dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export var __esModule: boolean;
-export { _default as CoursesHome };
